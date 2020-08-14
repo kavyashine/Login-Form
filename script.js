fde7cda07e963ca70c8b2ca8d9ad0899aed243de
kavyashine <68185911+kavyashine@users.noreply.github.com>
2020-08-14 15:22:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        items: 1
+    });
+});
